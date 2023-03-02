58018c5e464bfb055fb6c7d8cd90adee19bca9ef
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-02 16:05:00 +0200

    Створив розмітку галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+
+
+
+
+const galleryItem = galleryItems.map(({preview, description, original,}) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}"/>
+    </a>
+    </div>`).join('');
+galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
+// console.log(galleryEl);
+// console.log(galleryList);
