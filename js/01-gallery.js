fe2da54b0ff849afadb6107abebb6b552160fb36
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-04 23:22:26 +0200

    Додав Escape на закриття модалки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,34 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 
-
-
-
-
-const galleryItem = galleryItems.map(({preview, description, original,}) => 
-    `<div class="gallery__item">
+const galleryItem = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img class="gallery__image"
     src="${preview}" 
     data-source="${original}"
     alt="${description}"/>
     </a>
-    </div>`).join('');
+    </div>`
+  )
+  .join('');
 galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
 
-const handleImgClick = ((event) => {
-    event.preventDefault();
+const handleImgClick = event => {
+  event.preventDefault();
 
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
 
-    const originalImgUrl = event.target.dataset.source;
+  const originalImgUrl = event.target.dataset.source;
 
-    const modalWindow = basicLightbox.create(`
+  const modalWindow = basicLightbox.create(`
     <img src="${originalImgUrl}" alt="${event.target.alt}"/>
-    `)
-    modalWindow.show();
-})
+    `);
+  modalWindow.show();
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      modalWindow.close();
+    }
+  });
+};
 
 galleryEl.addEventListener('click', handleImgClick);
