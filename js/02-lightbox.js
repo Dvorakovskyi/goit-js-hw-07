bbd6cfd4f461dbd26d531930cb75d4711470fbfc
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-05 15:51:05 +0200

    Виконав завдання з бібліотекою SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryImg = galleryItems.map(({ preview, original, description, }) =>     
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+galleryEl.insertAdjacentHTML('afterbegin', galleryImg);
+
+new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
